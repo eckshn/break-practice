a53e6470e3718578695f5a093be7484d0b996207
Eckart Schneider <eckshn@gmail.com>
2021-04-09 14:34:13 -0400

    motor

--- src/main/java/com/team4099/bcc/motor/motorAnswers/Motor.java
+++ src/main/java/com/team4099/bcc/motor/motorAnswers/Motor.java
@@ -1,10 +1,9 @@
-package com.team4099.bcc.motorAnswers;
+package com.team4099.bcc.motor.motorAnswers;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
-import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Motor extends SubsystemBase {
@@ -16,16 +15,33 @@ public class Motor extends SubsystemBase {
 
     private CANSparkMax spark = new CANSparkMax(5, CANSparkMaxLowLevel.MotorType.kBrushless);
     private CANSparkMax sparkMax = new CANSparkMax(6, CANSparkMaxLowLevel.MotorType.kBrushless);
-    //Set the speed of a TalonSRX motor to spin at 50% and the other to spin the opposite direction at 100%
 
-    talon1.set(ControlMode.PercentOutput, 0.1);
-    //Set the speed of a CANSparkMax motor to spin at 30% and the other to spin the opposite direction at 25%
+    //This part needs to be in a method to work
+    private void method() {
+        //Set the speed of a TalonSRX motor to spin at 50% and the other to spin the opposite direction at 100%
 
-    //Make one of the CANSparkMax motors follow the other
+        talon1.set(ControlMode.PercentOutput, 0.1);
+        //Set the speed of a CANSparkMax motor to spin at 30% and the other to spin the opposite direction at 25%
 
-    //Make one of the CANSparkMax motors follow the other inverted
+        spark.set(-.25);
+        sparkMax.set(0.3);
+        //Make one of the CANSparkMax motors follow the other
 
+        spark.follow(sparkMax);
+        //Make one of the CANSparkMax motors follow the other inverted
+
+        spark.follow(sparkMax, true);
+    }
     //Create a method that would set both of the TalonSRX motors to the same speed
 
+    public void setTalonPower(double power) {
+        talon1.set(ControlMode.PercentOutput, power);
+        talon2.set(ControlMode.PercentOutput, power);
+    }
     //Create a method that would set a TalonSRX motor and a CANSparkMax motor to the same speed
+
+    public void setMotorPower(double power) {
+        talon1.set(ControlMode.PercentOutput, power);
+        spark.set(power);
+    }
 }
--- src/main/java/com/team4099/bcc/motor/motorPractice/Motor.java
+++ src/main/java/com/team4099/bcc/motor/motorPractice/Motor.java
@@ -1,23 +1,25 @@
-package com.team4099.bcc.motorPractice;
+package com.team4099.bcc.motor.motorPractice;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Motor {
     //Make a TalonSRX motor with an id of 10 and another with an id of 11
 
     //Make a CANSparkMax brushless motor with an id of 5 and another with an id of 6
 
-    //Set the speed of a TalonSRX motor to spin at 50% and the other to spin the opposite direction at 100%
+    //This part needs to be in a method to work (Why is that?)
+    private void method() {
+        //Set the speed of a TalonSRX motor to spin at 50% and the other to spin the opposite direction at 100%
 
-    //Set the speed of a CANSparkMax motor to spin at 30% and the other to spin the opposite direction at 25%
+        //Set the speed of a CANSparkMax motor to spin at 30% and the other to spin the opposite direction at 25%
 
-    //Make one of the CANSparkMax motors follow the other
+        //Make one of the CANSparkMax motors follow the other
 
-    //Make one of the CANSparkMax motors follow the other inverted
+        //Make one of the CANSparkMax motors follow the other inverted
 
+    }
     //Create a method that would set both of the TalonSRX motors to the same speed
 
     //Create a method that would set a TalonSRX motor and a CANSparkMax motor to the same speed
+
 }
